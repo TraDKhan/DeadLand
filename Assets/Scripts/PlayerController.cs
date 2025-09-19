fa486b957a58d7c43dee43029fcb92cd50a647db
TraDkhan <trandkhanh04@gmail.com>
2025-09-19 19:05:34 +0700

    Complete Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Attack Settings")]
+    public float attackCooldown = 0.5f;   // thời gian chờ giữa các đòn
+    public Transform attackPoint;         // vị trí trung tâm đòn đánh
+    public float attackRange = 0.5f;      // bán kính đòn đánh
+    public LayerMask enemyLayer;          // layer địch
+
+    private Animator animator;
+    private float lastAttackTime;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleAttack();
+    }
+
+    void HandleAttack()
+    {
+        // Nhấn phím tấn công (ví dụ: J hoặc chuột trái)
+        if (Input.GetKeyDown(KeyCode.J) && Time.time >= lastAttackTime + attackCooldown)
+        {
+            lastAttackTime = Time.time;
+            animator.SetTrigger("Attack");  // gọi animation tấn công
+
+            // Gọi hàm đánh sau 1 chút (nếu muốn sync với animation)
+            Invoke(nameof(PerformAttack), 0.1f);
+        }
+    }
+
+    void PerformAttack()
+    {
+        // Phát hiện enemy trong vùng đòn đánh
+        Collider2D[] hits = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+        foreach (Collider2D enemy in hits)
+        {
+            Debug.Log("Đánh trúng: " + enemy.name);
+            // TODO: gọi hàm nhận sát thương của enemy
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+        HandleAnimation();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void HandleInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Nếu có input cả hai trục -> ưu tiên trục vừa nhấn mạnh hơn
+        if (x != 0 && y != 0)
+        {
+            // Ưu tiên trục được nhấn gần nhất (trục vừa thay đổi)
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+                y = 0;
+            else
+                x = 0;
+        }
+
+        movement = new Vector2(x, y);
+    }
+
+    void MovePlayer()
+    {
+        Vector2 newPosition = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
+    }
+
+    void HandleAnimation()
+    {
+        bool isMoving = movement != Vector2.zero;
+
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetBool("IsMoving", isMoving);
+
+        // Lưu hướng di chuyển cuối cùng (dùng khi Idle)
+        if (isMoving)
+        {
+            animator.SetFloat("LastMoveX", movement.x);
+            animator.SetFloat("LastMoveY", movement.y);
+        }
+    }
+}
